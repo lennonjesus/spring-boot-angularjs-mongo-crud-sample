43bff4bb4d787f71956d45d525377d0be37dde58
Lennon Jesus <lennon.jesus@gmail.com>
2015-08-18 16:35:39 -0300

    Initial commit

---
+++ src/main/java/com/lennonjesus/app/MySpringBootTestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.lennonjesus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootTestProjectApplication.class, args);
+    }
+}
