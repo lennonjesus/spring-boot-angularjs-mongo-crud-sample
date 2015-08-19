5c4d98328a6ae18da5f0b171b91f4052fdc10d69
Lennon Jesus <lennon.jesus@gmail.com>
2015-08-19 13:06:32 -0300

    alterando teste no controller para utilizar groovy e deixa o codigo mais legal de escrever

--- src/main/java/com/lennonjesus/app/controller/MedicoRestController.java
+++ src/main/java/com/lennonjesus/app/controller/MedicoRestController.java
@@ -4,10 +4,13 @@ import com.lennonjesus.app.entity.Medico;
 import com.lennonjesus.app.service.IMedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  *
@@ -20,7 +23,7 @@ public class MedicoRestController {
 
     @RequestMapping(value = "/medico", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public void salvar(Medico medico) {
+    public void salvar(@Valid @RequestBody Medico medico) {
 
         medicoService.salvar(medico);
 
--- src/main/java/com/lennonjesus/app/entity/Medico.java
+++ src/main/java/com/lennonjesus/app/entity/Medico.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -19,6 +21,8 @@ public class Medico {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Size(min = 2, max = 100)
     @Column
     private String nome;
 
